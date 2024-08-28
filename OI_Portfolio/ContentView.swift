28b5b7370ebff7938ccc3649f6ff1590c6b7e8d1
olaeben <ibitoyewale@gmail.com>
2024-08-28 10:34:46 +0100

    Initial Commit

---
+++ OI_Portfolio/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OI_Portfolio
+//
+//  Created by Wale Ibitoye on 8/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OI_Portfolio/OI_PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  OI_PortfolioApp.swift
+//  OI_Portfolio
+//
+//  Created by Wale Ibitoye on 8/28/24.
+//
+
+import SwiftUI
+
+@main
+struct OI_PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
